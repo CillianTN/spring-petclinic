435dd0960a573eef7759579616f59ac8dc6332e9
CillianN <cillian.nolan@students.ittralee.ie>
2024-04-20 14:01:04 +0100

    Testing

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -12,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * Adding a line as a test to push to Git
  */
 
 package org.springframework.samples.petclinic;
